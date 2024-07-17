c6f78dae0101401baa5bc2976bb1ff5018e15fd9
Karolina <najman.karo@gmail.com>
2024-07-17 20:28:02 +0200

    set configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: false,
   e2e: {
+    baseUrl: 'https://autoplac.pl/',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
