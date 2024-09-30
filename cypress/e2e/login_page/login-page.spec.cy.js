ef55df97d6d6b2e571519c7e5288d46382e20578
Karolina <najman.karo@gmail.com>
2024-09-30 20:11:00 +0200

    checking redirect to the proper page

--- cypress/e2e/login_page/login-page.spec.cy.js
+++ cypress/e2e/login_page/login-page.spec.cy.js
@@ -1,2 +1,11 @@
 /// <reference types="cypress" />
 
+import { loginPageData } from "./login-page.data"
+
+describe ('Login page', () => {
+    it('should redirect to the proper page', () => {
+        cy.visit('/')
+        cy.get('.header__item--login').click()
+        cy.url().should('include', loginPageData.loginUrl)
+    })
+})
