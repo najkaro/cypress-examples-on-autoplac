cd7f4a5228ad4049bfd0548c71068bcc261721f4
Karolina <najman.karo@gmail.com>
2025-01-14 13:31:08 +0100

    data update

--- cypress/e2e/main_page_header/header.data.js
+++ cypress/e2e/main_page_header/header.data.js
@@ -2,16 +2,17 @@ export const headerData =
 {
     "blogButton": "Blog",
     "blogPage": "/blog",
+    "VinCheckButton": "Sprawdź VIN",
     "estimateCarButton": "Wyceń auto",
     "estimateCarPage": "/indicata",
     "financingButton": "Finansowanie",
     "financingPage": "https://autoplac.pl/finansowanie/",
     "postAdButton": "Wystaw za 0 zł",
     "postAdPage": "/wystaw",
-    "favoriteButton": "Dodaj do ulubionych",
+    "favoriteButton": "Ulubione",
     "favoritePage": "/ogloszenia/ulubione",
     "newsButton": "Aktualności",
     "logInButton": "Zaloguj",
-    "logInPage": "/auth/login",
+    "logInPage": "https://autoplac.pl/auth/login",
     
 }
